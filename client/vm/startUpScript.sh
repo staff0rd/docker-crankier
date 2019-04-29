7e0ad1b4128ed565f823a015ce6a8c281d43780b
stafford <stafford.williams@gmail.com>
2019-04-30 07:00:40 +1000

    vm client

---
+++ client/vm/startUpScript.sh
@@ -0,0 +1,9 @@
+## this script runs as root
+apt update
+apt install docker.io -y 
+apt install docker-compose -y
+
+# let admin user use docker without sudo
+gpasswd -a crankier docker
+
+#docker run -d -p 80:80 staff0rd/crankier-server
