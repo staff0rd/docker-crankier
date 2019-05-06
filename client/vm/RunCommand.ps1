3071a4d8d943e03f20694af5562c9ca7b7ef86e6
stafford <stafford.williams@gmail.com>
2019-05-06 22:14:30 +1000

    wip

--- client/vm/RunCommand.ps1
+++ client/vm/RunCommand.ps1
@@ -10,10 +10,14 @@ $password = "mypassword" | ConvertTo-SecureString -asPlainText -Force
 $username = "crankier"
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 
+Get-AzPublicIpAddress | select -ExpandProperty DnsSettings | select -Property Fqdn
+
 foreach ($vm in $vms) {
     if ($vm.Name.StartsWith($vmNamePrefix)) {
         $prefix = $vm.Name.replace("-vm", "")
         $fqdn = (Get-AzPublicIpAddress -Name "$prefix-ip" -ResourceGroupName $resourceGroupName).DnsSettings.Fqdn
+        #write-host $fqdn
+        Remove-SSHTrustedHost $fqdn
         $session = New-SSHSession -ComputerName $fqdn -KeyFile ~\.ssh\id_rsa -credential $credential -AcceptKey
         $result = Invoke-SSHCommand $session $command
         Write-Host $result.Output
