84380434e78a3fb3dceaeac48fd6f2c835f3fa07
stafford <stafford.williams@gmail.com>
2019-10-14 22:08:55 +1100

    server update

--- server/virtualMachine.sh
+++ server/virtualMachine.sh
@@ -6,4 +6,4 @@ apt install docker-compose -y
 # let admin user use docker without sudo
 gpasswd -a crankier docker
 
-docker run -d -p 80:80 --name crankier staff0rd/crankier server
+docker run -d -p 80:80 --name crankier -e ASPNETCORE_URLS=http://0.0.0.0:80 staff0rd/crankier server --log Warning
